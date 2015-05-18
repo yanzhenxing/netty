ccb19cacbebeda7ca4c4823a2e8f129e3bda7340
yanzhenxing <yanzhenxing1234@126.com>
2015-05-18 21:09:48 +0800

    echo 服务

---
+++ src/main/java/com/quauq/yanzhenxing/netty/example/echo/EchoServerHandler.java
@@ -0,0 +1,21 @@
+package com.quauq.yanzhenxing.netty.example.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.CharsetUtil;
+
+public class EchoServerHandler  extends ChannelHandlerAdapter {
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ctx.writeAndFlush(msg);
+	}
+
+}
